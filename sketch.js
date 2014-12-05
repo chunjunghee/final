cb82d7768a4eb6c26f98f2698c073bb2b562dbb1
chunjunghee <dkddkd0311@naver.com>
2014-12-05 17:34:17 +0900

    ff

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
 var bg;
-
-var img3;
-var img4;
 var img;
 var img2;
+var img3;
+var img4;
 var img5;
 
 var c;
@@ -15,10 +14,10 @@ function setup()
 
     createCanvas(1111,1111);
     bg = loadImage("bg.png");
-    img3 = loadImage("cloud1.png");
-    img4 = loadImage("cloud2.png");
     img = loadImage("earth.png");
     img2 = loadImage("plane.png");
+    img3 = loadImage("cloud1.png");
+    img4 = loadImage("cloud2.png");
     img5 = loadImage("balloon.png");
 
     }
@@ -28,30 +27,39 @@ function draw()
 {
     background(bg);
 
-    push();
-    translate(width*0.5, height*0.5);
-    rotate(frameCount / 200.0);
-    image(img,2-img.width/2,2-img.height/2);
-    pop();
 
-    push();
-    translate(width*0.5, height*0.5);
-    rotate(frameCount / 50.0);
-    image(img2,2-img2.width/2,2-img2.height/2);
-    pop();
 
+    //cloud1
     push();
     translate(width*0.5, height*0.5);
     rotate(frameCount / 400.0);
     image(img3,2-img3.width/2,2-img3.height/2);
     pop();
 
+
+    //cloud2
     push();
     translate(width*0.5, height*0.5);
-    rotate(frameCount / 400.0);
+    rotate(frameCount / 450.0);
     image(img4,2-img4.width/2,2-img4.height/2);
     pop();
+
+
+    //earth
+    push();
+    translate(width*0.5, height*0.5);
+    rotate(frameCount / 200.0);
+    image(img,2-img.width/2,2-img.height/2);
+    pop();
     
+
+    //plane
+    push();
+    translate(width*0.5, height*0.5);
+    rotate(frameCount / 50.0);
+    image(img2,2-img2.width/2,2-img2.height/2);
+    pop();
+
 
     if(f){
     if(c < -17){
