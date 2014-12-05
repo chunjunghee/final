aed602b8990e481c37ea3ed3ae71d20716a652ab
chunjunghee <dkddkd0311@naver.com>
2014-12-05 16:26:22 +0900

    gg

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var bg;
 var img;
+var img2;
 
 function setup()
 {
@@ -8,6 +9,7 @@ function setup()
     bg = loadImage("bg.png");
     bg = loadImage("bg.png");
     img = loadImage("earth.png");
+    img2 = loadImage("plane.png");
 
     }
 
@@ -20,6 +22,12 @@ function draw()
     translate(width*0.5, height*0.5);
     rotate(frameCount / 200.0);
     image(img,2-img.width/2,2-img.height/2);
+    pop();
+
+    push();
+    translate(width*0.5, height*0.5);
+    rotate(frameCount / 50.0);
+    image(img2,2-img.width/2,2-img.height/2);
     pop();
     
 
